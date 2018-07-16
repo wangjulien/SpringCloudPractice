9a8970352767aa17f092aa87aaa790c60425b262
Jiliang Wang <jiliang.wang@telino.com>
2018-07-16 16:41:37 +0200

    Add feign-client

--- ConfigurationClient/src/main/java/com/obbo/cloud/configclient/ConfigClientApplication.java
+++ ConfigurationClient/src/main/java/com/obbo/cloud/configclient/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package com.obbo.cloud.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 public class ConfigClientApplication {
 	
@@ -27,4 +29,9 @@ public class ConfigClientApplication {
 	public String whoami(@PathVariable("username") String username) {
 		return String.format("Hello ! You're %s and you'll become a %s, but only if your password is '%s' ...\n", username, role, password);
 	}
+	
+	@RequestMapping("/greeting")
+    public String greeting() {
+        return "Hello from EurekaClient!";
+    }
 }
--- GatewayServer/src/main/java/com/obbo/cloud/gateway/GatewayServerApplication.java
+++ GatewayServer/src/main/java/com/obbo/cloud/gateway/GatewayServerApplication.java
@@ -1,16 +1,16 @@
-package com.obbo.cloud.configserver;
+package com.obbo.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableZuulProxy
 @EnableEurekaClient
-public class ConfigServerApplication {
+public class GatewayServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 }
